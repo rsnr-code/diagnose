05d587028602b57184345bbcf99c7e8c1077e866
rsnr-code <rishadnero@gmail.com>
2022-09-23 14:03:35 -0400

    connected database

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const connectDB = require("./config/db");
 
 const mainRoutes = require("./routes/main");
 
@@ -7,12 +10,18 @@ const mainRoutes = require("./routes/main");
 //Use .env file in config folder
 require("dotenv").config({ path: "./config/.env" });
 
+
+//Connect To Database
+connectDB();
+
 //Using EJS for views
 app.set("view engine", "ejs");
 
 //Static Folder
 app.use(express.static("public"));
 
+//Logging
+app.use(logger("dev"));
 
 //Setup Routes For Which The Server Is Listening
 app.use("/", mainRoutes);
