028a6ea4f43db16168da18ab0c927ac71c8a2f88
rsnr-code <rishadnero@gmail.com>
2022-09-23 11:49:53 -0400

    added index.ejs file with routes & controllers files

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
 
+const mainRoutes = require("./routes/main");
 
 
 //Use .env file in config folder
 require("dotenv").config({ path: "./config/.env" });
+
+
+//Setup Routes For Which The Server Is Listening
+app.use("/", mainRoutes);
+
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on PORT ${process.env.PORT}`)
